8074cbbfacf8bc3e6a69cec68c898b80d01f125f
Kaic Bastidas <kaicbastidas@gmail.com>
2020-07-26 19:23:10 -0300

    reafactor: update custom document signature

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,27 +4,31 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
+    const documentInitialProps = await Document.getInitialProps(ctx);
+
+    const finalProps = {
+      ...documentInitialProps,
+    };
+
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
 
     try {
+      const originalRenderPage = ctx.renderPage;
+
       ctx.renderPage = () => originalRenderPage({
         enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
       });
 
-      const initialProps = await Document.getInitialProps(ctx);
-
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
+      finalProps.styles = (
+        <>
+          {finalProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      );
     } finally {
       sheet.seal();
     }
+
+    return finalProps;
   }
 }
